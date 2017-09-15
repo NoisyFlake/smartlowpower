fb43ed9160afe06bd6ffc5c6d7791a77ee2320f5
Joel Poick <joel.poick@cycos.com>
2017-09-15 15:00:55 +0200

    Increase version to 1.1, add FlipSwitch as dependency

--- smartlowpowerprefs/slpRootListController.m
+++ smartlowpowerprefs/slpRootListController.m
@@ -74,7 +74,7 @@
 		version.textColor = [UIColor colorWithRed:0.3f green:0.3f blue:0.3f alpha:1.0f];
 		version.shadowColor = [UIColor whiteColor];
 		version.shadowOffset = CGSizeMake(0.0f, 1.0f);
-		version.text = @"Version 1.0.0";
+		version.text = @"Version 1.1";
 		version.backgroundColor = [UIColor clearColor];
 		version.textAlignment = NSTextAlignmentCenter;
 
